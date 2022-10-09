73f76bc5fb93e49c02a14b5a5fd6912aaa000ccd
Park <zalop3128@kakao.com>
2022-10-10 05:59:01 +0900

    FEAT : [013-ADD-CART] 카트에 대한 추가 구현

---
+++ order-api/src/main/java/com/zerobase/cms/order/application/CartApplication.java
@@ -0,0 +1,166 @@
+package com.zerobase.cms.order.application;
+
+
+import com.zerobase.cms.order.domain.model.Product;
+import com.zerobase.cms.order.domain.model.ProductItem;
+import com.zerobase.cms.order.domain.product.AddProductCartForm;
+import com.zerobase.cms.order.domain.redis.Cart;
+import com.zerobase.cms.order.exception.CustomException;
+import com.zerobase.cms.order.exception.ErrorCode;
+import com.zerobase.cms.order.service.CartService;
+import com.zerobase.cms.order.service.ProductSearchService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+@Service
+@RequiredArgsConstructor
+public class CartApplication {
+    private final ProductSearchService productSearchService;
+    private final CartService cartService;
+
+    public Cart addCart(Long customerId, AddProductCartForm form) {
+
+        Product product = productSearchService.getByProductId(form.getId());
+
+        if (product == null) {
+            throw new CustomException(ErrorCode.NOT_FOUND_PRODUCT);
+        }
+        Cart cart = cartService.getCart(customerId);
+
+        if (!addAble(cart, product, form)) {
+            throw new CustomException(ErrorCode.ITEM_COUNT_NOT_ENOUGH);
+        }
+        return cartService.addCart(customerId, form);
+    }
+
+    public Cart updateCart(Long customerId, Cart cart){
+        //실질적으로 변하는 데이터
+        // 상품의 삭제, 수량 변경
+        cartService.putCart(customerId, cart);
+
+        return getCart(customerId);
+
+    }
+
+    private boolean addAble(Cart cart, Product product, AddProductCartForm form) {
+        Cart.Product cartProduct = cart.getProducts().stream().filter(p -> p.getId().equals(form.getId()))
+                .findFirst().orElse(Cart.Product.builder().id(product.getId())
+                        .items(Collections.emptyList()).build());
+
+        Map<Long, Integer> cartItemCountMap = cartProduct.getItems().stream()
+                .collect(Collectors.toMap(Cart.ProductItem::getId, Cart.ProductItem::getCount));
+
+        Map<Long, Integer> currentItemCountMap = product.getProductItems().stream()
+                .collect(Collectors.toMap(ProductItem::getId, ProductItem::getCount));
+
+        return !form.getItems().stream().noneMatch(
+                formItem -> {
+                    Integer cartCount = cartItemCountMap.get(formItem.getId());
+                    if(cartCount == null){
+                        cartCount = 0;
+                    }
+                    Integer currentCount = currentItemCountMap.get(formItem.getId());
+                    return formItem.getCount() + cartCount > currentCount;
+                });
+    }
+
+    //1. 장바구니에 상품을 추가 했다.
+    //2. 상품의 가격이나 수량이 변동이 된다.
+    public Cart getCart(Long customerId) {
+        Cart cart = refreshCart(cartService.getCart(customerId));
+        cartService.putCart(cart.getCustomerId(), cart);
+        Cart returnCart = new Cart();
+        returnCart.setCustomerId(customerId);
+        returnCart.setProducts(cart.getProducts());
+        returnCart.setMessages(cart.getMessages());
+
+        cart.setMessages(new ArrayList<>());
+
+        cartService.putCart(customerId, cart);
+        return returnCart;
+        //2. 메세지를 보고 난 다음에는, 이미 본 메세지는 스팸이 되기 때문에 제거한다.
+    }
+
+    public void clearCart(Long customerId){
+        cartService.putCart(customerId, null);
+    }
+
+    protected Cart refreshCart(Cart cart) {
+        //1. 상품이나 상품의 아이템의 정보, 가격, 수량이 변경되었는지 체크 후
+        // 그에 맞는 알람을 제공한다.
+        //2. 상품의 수량, 가격을 우리가 임의로 변경한다.
+        Map<Long, Product> productMap = productSearchService.getListByProductIds(cart.getProducts().stream().map(Cart.Product::getId).collect(Collectors.toList()))
+                .stream().collect(Collectors.toMap(Product::getId, product -> product));
+
+        for (int i = 0; i < cart.getProducts().size(); i++) {
+            Cart.Product cartProduct = cart.getProducts().get(i);
+            Product p = productMap.get(cartProduct.getId());
+            if (p == null) {
+                cart.getProducts().remove(cartProduct);
+                i--;
+                cart.addMessage(cartProduct.getName() + "상품이 삭제되었습니다.");
+                continue;
+            }
+
+            Map<Long, ProductItem> productItemMap = p.getProductItems().stream()
+                    .collect(Collectors.toMap(ProductItem::getId, productItem -> productItem));
+
+            List<String> tmpMessages = new ArrayList<>();
+
+            for (int j = 0; j < cartProduct.getItems().size(); j++) {
+                Cart.ProductItem cartProductItem = cartProduct.getItems().get(j);
+                ProductItem pi = productItemMap.get(cartProductItem.getId());
+                if (pi == null) {
+                    cartProduct.getItems().remove(cartProductItem);
+                    j--;
+                    tmpMessages.add(cartProductItem.getName() + "옵션이 삭제되었습니다.");
+                    continue;
+                }
+
+
+                boolean isPriceChanged = false, isCountNotEnough = false;
+                if (!cartProductItem.getPrice().equals(pi.getPrice())) {
+                    isPriceChanged = true;
+                    cartProductItem.setPrice(pi.getPrice());
+                }
+                if (cartProductItem.getCount() > pi.getCount()) {
+                    isCountNotEnough = true;
+                    cartProductItem.setCount(pi.getCount());
+                }
+                if (isCountNotEnough && isCountNotEnough) {
+                    //message1
+                    tmpMessages.add(cartProductItem.getName() + "가격변동, 수량이 부족하여 구매 가능한 최대치로 변경되었습니다.");
+                } else if (isPriceChanged) {
+                    //message2
+                    tmpMessages.add(cartProductItem.getName() + "가격이 변동되었습니다.");
+                } else if (isCountNotEnough) {
+                    //message3
+                    tmpMessages.add(cartProductItem.getName() + "수량이 부족하여 구매 가능한 최대치로 변경되었습니다.");
+                }
+            }
+            if (cartProduct.getItems().size() == 0) {
+                cart.getProducts().remove(cartProduct);
+                i--;
+                cart.addMessage(cartProduct.getName() + "상품의 옵션이 모두 없어져 구매가 불가능합니다.");
+                continue;
+            }else if (tmpMessages.size() > 0) {
+                StringBuilder builder = new StringBuilder();
+                builder.append(cartProduct.getName() + " 상품의 변동 사항 : ");
+                for (String message : tmpMessages) {
+                    builder.append(message);
+                    builder.append(", ");
+                }
+                cart.addMessage(builder.toString());
+            }
+        }
+
+//        cartService.putCart(cart.getCustomerId(), cart);
+        return cart;
+    }
+}
---
+++ order-api/src/main/java/com/zerobase/cms/order/controller/CustomerCartController.java
@@ -0,0 +1,57 @@
+package com.zerobase.cms.order.controller;
+
+
+import com.zerobase.cms.order.application.CartApplication;
+import com.zerobase.cms.order.application.OrdersApplication;
+import com.zerobase.cms.order.domain.product.AddProductCartForm;
+import com.zerobase.cms.order.domain.redis.Cart;
+import com.zerobase.domain.config.JwtAuthenticationProvider;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/customer/cart")
+@RequiredArgsConstructor
+public class CustomerCartController {
+
+    //임시 코드
+    private final CartApplication cartApplication;
+    private final JwtAuthenticationProvider provider;
+
+    private final OrdersApplication orderApplication;
+
+    @PostMapping
+    public ResponseEntity<Cart> addCart(
+            @RequestHeader(name = "X-AUTH-TOKE") String token,
+            @RequestBody AddProductCartForm form) {
+        return ResponseEntity.ok(cartApplication.addCart(provider.getUserVo(token).getId(), form));
+    }
+
+
+    @GetMapping
+    public ResponseEntity<Cart> showCart(
+            @RequestHeader(name = "X-AUTH-TOKE") String token) {
+        return ResponseEntity.ok(cartApplication.getCart(provider.getUserVo(token).getId()));
+
+    }
+
+    @PutMapping
+    public ResponseEntity<Cart> updateCart(
+            @RequestHeader(name = "X-AUTH-TOKE") String token,
+            @RequestBody Cart cart) {
+
+        return ResponseEntity.ok(cartApplication.updateCart(provider.getUserVo(token).getId(), cart));
+    }
+
+
+
+    @PostMapping("/order")
+    public ResponseEntity<Cart> order(
+            @RequestHeader(name = "X-AUTH-TOKE") String token,
+            @RequestBody Cart cart) {
+        orderApplication.order(token, cart);
+        return ResponseEntity.ok().build();
+    }
+
+}
